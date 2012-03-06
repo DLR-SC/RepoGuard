6ea10b0e2263216b7fc30650fc92a65af596de23
Tobias Schlauch <schlauch@users.noreply.github.com>
2012-03-06 18:12:55 +0000

    Fixes ISSUE #24: - Added doc command which creates the Sphinx documentation

--- repoguard/setup.py
+++ repoguard/setup.py
@@ -18,12 +18,14 @@ Setup script for the RepoGuard distribution.
 """
 
 
-import sys
+from distutils import core
 import os
+import sys
+import subprocess
 
 from distribute_setup import use_setuptools
 use_setuptools()
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, command
 
 
 _WIN32_CONFIG_HOME = os.path.join(os.path.expanduser("~"), ".repoguard")
@@ -31,16 +33,45 @@ _LINUX_CONFIG_HOME = "/usr/local/share/repoguard"
 CONFIG_HOME = _WIN32_CONFIG_HOME if sys.platform == "win32" else _LINUX_CONFIG_HOME
 CONFIG_HOME = os.getenv("REPOGUARD_CONFIG_HOME", CONFIG_HOME)
 DEBUG = os.getenv("REPOGUARD_DEBUG")
-
-# Renames the main repoguard script for test purposes
-# to prevent that the current repoguard start script is overridden
 _CONSOLE_SCRIPTS = "repoguard = repoguard.main:main"
-if DEBUG:
+if DEBUG: # Adds a debug prefix to allow test with new version without de-activating the old one
     _CONSOLE_SCRIPTS = "repoguard-debug = repoguard.main:main"
+
+
+class doc(core.Command):
+    """ Creates the developer documentation. """
+    
+    description = "Creates the developer documentation."
+    user_options = [("command=", None, "Path and name of 'sphinx-build' command. Default: sphinx-build"),
+                     ("destdir=", None, "Path to directory which should contain the documentation.")]
+    _COMMAND_TEMPLATE = "{0} doc/source doc/html"
+
+    
+    def __init__(self, distribution):
+        self.verbose = None
+        self.command = None
+
+        core.Command.__init__(self, distribution)
+
+    def initialize_options(self):
+        self.verbose = False
+        self.command = "sphinx-build"
+        if sys.platform == "win32":
+            self.command += ".exe"
+
+    def finalize_options(self):
+        self.verbose = self.distribution.verbose
+        
+    def run(self):
+        command = self._COMMAND_TEMPLATE.format(self.command )
+        if self.verbose:
+            print(command)
+        subprocess.call(command)
 
 setup(
     name="repoguard", 
     version="0.3.0-dev",
+    cmdclass={"doc": doc},
     description="RepoGuard is a framework for Subversion hook scripts.",
     long_description="RepoGuard is a framework for Subversion pre-commit hooks in order to implement checks of the to be commited files before they are commited. For example, you can check for the code style or unit tests. The output of the checks can be send by mail or be written into a file or simply print to the console..",
     author="Deutsches Zentrum fuer Luft- und Raumfahrt e.V. (DLR)",
