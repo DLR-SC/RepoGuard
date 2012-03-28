c8b6517ceef49b9cbe6792a30e176eafee0dd8a8
Tobias Schlauch <schlauch@users.noreply.github.com>
2012-03-28 20:54:57 +0000

    ISSUE #36: Refactored the protocol tests.

--- repoguard/test/repoguard_test/core/test_protocol.py
+++ repoguard/test/repoguard_test/core/test_protocol.py
@@ -1,5 +1,3 @@
-# pylint: disable=R0201
-# R0201: Cannot make test methods static as nose would not find them anymore. 
 #
 # Copyright 2008 German Aerospace Center (DLR)
 #
@@ -21,105 +19,72 @@
 
 from repoguard.core import constants
 from repoguard.core.protocol import Protocol, ProtocolEntry
-from repoguard_test import util
 
 
 class TestProtocol(object):
-    """ Implements tests of the protocol handler. """
+    
+    def setup_method(self, _):
+        self._protocol = Protocol("test")
+        self._entry = ProtocolEntry("dummy", dict(), constants.SUCCESS, "dummy")
+        self._protocol.append(self._entry)
     
     def test_properties(self):
-        """ Tests the property access. """
+        assert self._protocol.success == True
+        assert self._protocol.result == constants.SUCCESS
         
-        protocol = Protocol("dummy")
+        entry = ProtocolEntry("dummy", dict(), constants.ERROR,"dummy")
+        self._protocol.append(entry)
+        assert self._protocol.success == False
         
-        entry = ProtocolEntry("dummy", {}, constants.SUCCESS,"dummy")
-        protocol.append(entry)
-        assert protocol.success == True
+        entry = ProtocolEntry("dummy", dict(), constants.SUCCESS,"dummy")
+        self._protocol.append(entry)
+        assert self._protocol.success == False
         
-        entry = ProtocolEntry("dummy", {}, constants.ERROR,"dummy")
-        protocol.append(entry)
-        assert protocol.success == False
-        
-        entry = ProtocolEntry("dummy", {}, constants.SUCCESS,"dummy")
-        protocol.append(entry)
-        assert protocol.success == False
-        
-        assert protocol.errors == 1
-        assert protocol.successors == 2
-        assert protocol.warnings == 0
-        assert protocol.exceptions == 0
-        
+        assert self._protocol.errors == 1
+        assert self._protocol.successors == 2
+        assert self._protocol.warnings == 0
+        assert self._protocol.exceptions == 0
+        assert self._protocol.result == constants.ERROR
+        assert str(self._protocol) != None
+
     def test_filter(self):
-        """ Tests the filter method. """
-               
-        protocol = Protocol("test")
-        entry = ProtocolEntry("dummy", {}, constants.SUCCESS,"dummy")
-        protocol.append(entry)
-        
-        assert len(protocol.filter(None, None)) == 1
-        assert len(protocol.filter(["dummy"], None)) == 1
-        assert len(protocol.filter(None, ["dummy"])) == 0
-        assert len(protocol.filter(["dummy"], [])) == 1
-        assert len(protocol.filter([], ["dummy"])) == 0
-        assert len(protocol.filter([], [])) == 0
+        assert len(self._protocol.filter(None, None)) == 1
+        assert len(self._protocol.filter(["dummy"], None)) == 1
+        assert len(self._protocol.filter(None, ["dummy"])) == 0
+        assert len(self._protocol.filter(["dummy"], list())) == 1
+        assert len(self._protocol.filter(list(), ["dummy"])) == 0
+        assert len(self._protocol.filter(list(), list())) == 0
         
     def test_clear(self):
-        """ Tests the clear method. """
+        assert len(self._protocol) == 1
         
-        protocol = Protocol("test")
-        entry = ProtocolEntry("dummy", {}, constants.SUCCESS,"dummy")
-        protocol.append(entry)
-        
-        assert len(protocol) == 1
-        
-        protocol.clear()
-        assert len(protocol) == 0
+        self._protocol.clear()
+        assert len(self._protocol) == 0
 
 
 class TestProtocolEntry(object):
-    """ Implements tests for the protocol entries. """
         
-    @classmethod
-    def setup_class(cls):
-        """ Creates the test setup. """
+    def setup_method(self, _):
+        self._entry = ProtocolEntry("dummy", dict(), constants.ERROR,"dummy")
         
-        cls.entry = ProtocolEntry("dummy", dict(), constants.ERROR,"dummy")
-        
-    def test_attributes(self):
-        """ Checks the properties. """
-        
-        assert self.entry.duration == 0
-        assert self.entry.check == "dummy"
-        assert self.entry.msg == "dummy"
-        assert self.entry.config == {}
-        assert self.entry.result == constants.ERROR
-        assert self.entry.success == False
-        
-        print self.entry
+    def test_properties(self):
+        assert self._entry.duration == 0
+        assert self._entry.check == "dummy"
+        assert self._entry.msg == "dummy"
+        assert self._entry.config == dict()
+        assert self._entry.result == constants.ERROR
+        assert self._entry.success == False
+        assert str(self._entry) != None
         
     def test_is_included(self):
-        """ Tests the included flag. """
-        
-        entry = util.TestProtocolEntry.create(check="dummy")
-        assert entry.is_included(None, None) == True
-        assert entry.is_included(["dummy"], None) == True
-        assert entry.is_included(None, ["dummy"]) == False
-        assert entry.is_included(["dummy"], []) == True
-        assert entry.is_included([], ["dummy"]) == False
-        assert entry.is_included([], []) == False
-        
-        entry = util.TestProtocolEntry.create(check="foo")
-        assert entry.is_included(None, None) == True
-        assert entry.is_included(["dummy"], None) == False
-        assert entry.is_included(None, ["dummy"]) == True
-        assert entry.is_included(["dummy"], []) == False
-        assert entry.is_included([], ["dummy"]) == False
-        assert entry.is_included([], []) == False
+        assert self._entry.is_included(None, None) == True
+        assert self._entry.is_included(["dummy"], None) == True
+        assert self._entry.is_included(None, ["dummy"]) == False
+        assert self._entry.is_included(["dummy"], list()) == True
+        assert self._entry.is_included(list(), ["dummy"]) == False
+        assert self._entry.is_included(list(), list()) == False
         
     def test_duration(self):
-        """ Tests the duration property. """
-        
-        entry = util.TestProtocolEntry.create(check="dummy")
-        entry.start_time = 0.5
-        entry.end_time = 1.5
-        assert entry.duration == 1000
+        self._entry.start_time = 0.5
+        self._entry.end_time = 1.5
+        assert self._entry.duration == 1000
