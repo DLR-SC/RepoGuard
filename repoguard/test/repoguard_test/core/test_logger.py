a6ac7127ff8f28072d6ef25743ea43eb9ad71f6c
Tobias Schlauch <schlauch@users.noreply.github.com>
2012-03-29 20:05:16 +0000

    ISSUE #36: Separated the test cases.

--- repoguard/test/repoguard_test/core/test_logger.py
+++ repoguard/test/repoguard_test/core/test_logger.py
@@ -28,7 +28,7 @@ from repoguard.core.logger import LoggerFactory
 
 
 _LOGGER_CONFIG = """
-output = %s
+output = /path/to/log/dir
 default = ERROR
 
 repoguard.core.checker = FAILURE
@@ -36,7 +36,7 @@ repoguard.core.validator = INFO
 
 repoguard.foo.bar = 42
 
-repoguard.interpolation = ${default}
+repoguard.interpolation = ${repoguard.foo.bar}
 """
 
 
@@ -46,24 +46,29 @@ class TestLoggerFactory(object):
     def setup_class(cls):
         cls.factory = LoggerFactory(config=_LOGGER_CONFIG.splitlines())
         
-    def test_create(self):
+    def test_create_default(self):
         logger = self.factory.create()
         assert logger.level == logging.ERROR
-        
+    
+    def test_create_undefined(self):
         logger = self.factory.create("DEFAULT")
         assert logger.level == logging.ERROR
         
+    def test_create_defined(self):
         logger = self.factory.create("repoguard.core.validator")
         assert logger.level == logging.INFO
-        
-        logger = self.factory.create("repoguard.core.validator", override=logging.NOTSET)
-        assert logger.level == logging.NOTSET
         
         logger = self.factory.create("repoguard.foo.bar")
         assert logger.level == 42
         
+    def test_create_override(self):
+        logger = self.factory.create("repoguard.core.validator", override=logging.NOTSET)
+        assert logger.level == logging.NOTSET
+        
+    def test_create_unknown_error_level(self):
         with pytest.raises(ValueError): # pytest.raises exists pylint: disable=E1101
             self.factory.create("repoguard.core.checker")
         
+    def test_create_from_variable_def(self):
         logger = self.factory.create("repoguard.interpolation")
-        assert logger.level == logging.ERROR
+        assert logger.level == 42
