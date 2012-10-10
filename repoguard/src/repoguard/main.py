925575de46a7cea2e2c1911f2184b6257ddb5ea6
Tobias Schlauch <schlauch@users.noreply.github.com>
2012-10-10 21:07:46 +0000

    Corrected loading of entry points for command line tools. Now separate projects can define the entry point "repoguard.tools" and RepoGuard finds the corresponding sub-command.

--- repoguard/src/repoguard/main.py
+++ repoguard/src/repoguard/main.py
@@ -37,15 +37,18 @@ def main(argv=sys.argv):
     
     usage = "\n  repoguard [options] mode\n\n" \
           + "Following 'mode'-values are accepted:\n"
+          
+    tools = dict()
     
-    group = 'repoguard.tools'
-    tools = {}
-    names = pkg_resources.get_entry_map('repoguard', group).keys()
-    names.sort(cmp=lambda x,y: cmp(x.lower(), y.lower()))
+    entrypoints = dict()
+    for entrypoint in pkg_resources.iter_entry_points("repoguard.tools"):
+        entrypoints[entrypoint.name] = entrypoint.load()()
+    names = entrypoints.keys()
+    names.sort(cmp=lambda x, y: cmp(x.lower(), y.lower()))
     for name in names:
-        tool = pkg_resources.load_entry_point('repoguard', group, name)()
+        tool = entrypoints[name]
         descriptors = tool.descriptors.items()
-        descriptors.sort(cmp=lambda x,y: cmp(x[0].lower(), y[0].lower()))
+        descriptors.sort(cmp=lambda x, y: cmp(x[0].lower(), y[0].lower()))
         
         usage += "%-18s\t%s\n" % (name, tool.description)
         for command, descriptor in descriptors:
@@ -56,7 +59,6 @@ def main(argv=sys.argv):
             tools[command] = (tool, descriptor)
         
     usage += "Write repoguard mode -h to get more help for every option"
-    
     version = pkg_resources.require("repoguard")[0].version
     full_name = "RepoGuard Command Line Tools v" + version
     parser = OptionParser(usage=usage, version=full_name)
